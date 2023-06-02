f7158a3ffbfb3a7be0bb8d0c80ca8999fab9a017
sushil76 <119693099+sushil76@users.noreply.github.com>
2023-06-02 06:11:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-global.mods = ['16122546569','32460248586'] 
-global.prems = ['16122546569', '32460248586', ]
+global.mods = ['+16122546569','32460248586'] 
+global.prems = ['+16122546569', '32460248586', ]
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
